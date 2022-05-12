93a6cca4329601e618d6dccef9ef1ace67a6b37a
NO NAME 07 <103588608+No-Name-07@users.noreply.github.com>
2022-05-12 20:14:46 +0700

    Add files via upload

---
+++ cctv.py
@@ -0,0 +1,84 @@
+
+import os
+import random
+import sys
+import time
+from time  import sleep
+os.system('clear')
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() + 0.4)
+print ('Loading..')
+sleep(0.1)
+mengetik('> > > ] 100%')
+sleep(1)
+os.system('clear')
+def main():
+   password = input ('MASUKAN KATA SANDI LOGIN : ')
+   if password == 'noname':
+        print ('sukses login')
+        sleep(2)
+        os.system('clear')
+   else:
+        print ('password salah' )
+main()
+sleep(1)
+os.system('clear')
+#logo
+print ('\033[34m')
+print ('____ ____ _______     __')
+sleep(0.1)
+print (' / ___/ ___|_   _\ \   / /')
+sleep(0.1)
+print ('| |  | |     | |  \ \ / /')
+sleep(0.1)
+print ('| |__| |___  | |   \ V /')
+sleep(0.1)
+print (' \____\____| |_|    \_/')
+sleep(0.1,)
+print ('••••••••••••••••••••••••••••••••••••••••••••')
+print ('fb https://www.facebook.com/Takegon.07')
+print (' ig ___no__name_07x')
+print (' yt NO NAME 07 XXX')
+print ('••••••••••••••••••••••••••••••••••••••••••••')
+print (' script sad')
+
+
+print ('[1] satu dunia')
+print ('[2] keluar')
+
+def main():
+    pilihan = input ('Masukan pilihan : ')
+    if pilihan == '1' or pilihan == '01':
+        os.system('clear')
+        print ('loading..')
+        mengetik('> > > ] 100%')
+        print ('http://194.233.87.224/watch/53036929.html')
+        print ('http://194.233.87.224/watch/53842907.html')
+        print ('http://194.233.87.224/watch/38267441.html')
+        print ('http://194.233.87.224/watch/69645599.html')
+        print ('http://194.233.87.224/page/lang/indonesia/13')
+        print ('http://194.233.87.224/watch/51891273.html')
+        print ('http://194.233.87.224/watch/3100895.html')
+        print ('http://194.233.87.224/watch/55542247.html')
+        print ('http://194.233.87.224/watch/53553149.html')
+        print ('http://194.233.87.224/watch/54118329.html')
+        print ('http://194.233.87.224/watch/64374803.html')
+        print ('http://194.233.87.224/watch/57066597.html')
+        print ('http://194.233.87.224/watch/39570445.html')
+        print ('http://194.233.87.224/watch/30907293.html')
+        print ('http://194.233.87.224/watch/39966097.html')
+        print ('http://194.233.87.224/watch/49914215.html')
+        print ('http://194.233.87.224/watch/69379409.html')
+        print ('http://194.233.87.224/watch/33105807.html')
+        print ('http://194.233.87.224/watch/45834213.html')
+        print ('http://194.233.87.224/watch/46051121.html')
+        print ('http://194.233.87.224/watch/46050659.html')
+        print ('MARI KITA SERANG BERSAMA SAMA MENGHANCUR KANNYA ')
+        print ('http://194.233.87.224/?id=blowjob')
+        print ('http://194.233.87.224/?id=teen')
+        print ('http://194.233.87.224/?id=casting')
+        print ('http://194.233.87.224/watch/8343248.html')
+main()
